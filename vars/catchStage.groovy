a705ae1b8c815dda4ac187e9505bc742f892bd23
stefan <think@hotmail.de>
2016-08-21 21:24:52 +0200

    detect parallel

--- vars/catchStage.groovy
+++ vars/catchStage.groovy
@@ -1,4 +1,16 @@
 def call(String name, Closure body) {
+  try {
+    stage name
+  } catch(hudson.AbortException error) {
+    // probably called within a parallel step, so it's okay that it fails here
+    // TODO: find a better solution if we are in a parallel branch or not
+  }
+  mayFail(name) {
+    body()
+  }
+}
+
+def call(String name, Closure body) {
   stage name
   mayFail(name) {
     body()
