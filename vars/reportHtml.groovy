4fbe4e3d2415a50d04460cd98e226e721a33470e
stefan <think@hotmail.de>
2016-08-21 20:33:46 +0200

    simplified

--- vars/reportHtml.groovy
+++ vars/reportHtml.groovy
@@ -1,3 +1,6 @@
-def call(name, path) {
-  publishHTML(target: [reportDir:folder, reportFiles:'index.html', reportName: name])
+def call(Map map) {
+  assert map.path != null
+  assert map.name != null
+
+  publishHTML(target: [reportDir: map.path, reportFiles: 'index.html', reportName: map.name])
 }
